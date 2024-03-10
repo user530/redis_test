55fc365255b4c654ff70cca787553a53195914d1
user530 <m.timarsuev@gmail.com>
2024-03-10 22:49:47 +0300

    Extended users route, added 'photos' endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,26 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Test endpoints to get some photos to test caching
+router.get('/photos', async (req,res) => {
+  const albumId = req.query.albumId;
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/photos',
+    {params: {albumId}}
+  );
+
+  res.json(data);
+});
+
+router.get('/photos/:id', async (req,res) => {
+  const { id } = req.params;
+
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/photos/' + id
+  );
+
+  res.json(data);
+})
 
 module.exports = router;
